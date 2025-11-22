5e7599b1ae851ee9cca99d044e832dee3a4e470d
João Pedro <joaofg0912@gmail.com>
2025-11-22 18:47:55 -0300

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,15 @@
 // No arquivo: src/components/Layout.js
-
 import React from 'react';
-// O "Outlet" é um espaço reservado onde o React Router irá colocar
-// a página que o usuário está visitando (ex: Home, Cadastro).
-import { Outlet } from 'react-router-dom'; 
-import Header from './Header'; // Importa o cabeçalho que acabamos de criar
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 function Layout() {
   return (
     <div>
       <Header />
       <main>
-        {/* O "Outlet" renderiza o componente da rota atual */}
-        <Outlet /> 
+        <Outlet />
       </main>
-      {/* Você pode adicionar um <Footer /> aqui mais tarde */}
     </div>
   );
 }
